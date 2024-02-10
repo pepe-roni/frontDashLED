d3dd9741f9dd794ba8bb75bf33c515a298d0c63a
Derrick Ng <ahde2k@gmail.com>
2024-02-10 03:50:51 -0800

    Add files via upload

--- TeslaGPSLed_DeadReckoning/TeslaGPSLed_DeadReckoning.ino
+++ TeslaGPSLed_DeadReckoning/TeslaGPSLed_DeadReckoning.ino
@@ -3,7 +3,7 @@
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> 
 
 #define LED_COUNT 144 //number of leds in that array
-#define LED_PIN 32 //rgb led attached to D3
+#define LED_PIN 32 //rgb led attached to D32
 #define SERIALECHO true
 
 SFE_UBLOX_GNSS myGNSS;
@@ -14,15 +14,15 @@ uint16_t hueValue = 0;
 
 void setup()
 {
-  FastLED.addLeds<NEOPIXEL,4>(leds, LED_COUNT);
+  FastLED.addLeds<NEOPIXEL,LED_PIN>(leds, LED_COUNT);
   Wire.begin(22,23);
   Wire.setClock(400000);
-  
+
   Serial.begin(500000);
   while (!Serial);
   Serial.println(F("Tesla GPS DR LEDs"));
-  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
-  {
+
+  if (myGNSS.begin() == false){ //Connect to the u-blox module using Wire port
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1);
   }
@@ -43,17 +43,14 @@ void setup()
       Serial.println(F("Please see the previous example for more information."));
     }
   }
+
   myGNSS.setAutoHNRATT(false); //Make sure auto HNR attitude messages are disabled
   myGNSS.setAutoHNRINS(false); //Make sure auto HNR vehicle dynamics messages are disabled
   myGNSS.setAutoHNRPVT(false); //Make sure auto HNR PVT messages are disabled
 
-  myGNSS.setNavigationFrequency(30); //Set output to 5 times a second
+  // myGNSS.setNavigationFrequency(30); //Set output to 5 times a second
   myGNSS.setHNRNavigationRate(30);
-  uint8_t rate = myGNSS.getNavigationFrequency(); //Get the update rate of this module
-
-  Serial.print("Current update rate: ");
-  Serial.println(rate);
-
+  Serial.println("Current update rate: 30Hz");
   startupAnimation();
 }
 
@@ -67,50 +64,38 @@ void loop()
     Serial.print(millis()-lastTime);
     lastTime = millis(); //Update the timer
 
-    // float velocity = myGNSS.getGroundSpeed();
-    // Serial.print(F(" | Ground Speed (MPH): "));
-    // Serial.print(velocity/447.04);
 
-    if (myGNSS.getHNRDyn(125) == true) // Request HNR Dyn data using a 125ms timeout
-    {
-      Serial.print(F(" yAccel: "));
-      Serial.print(myGNSS.packetUBXHNRINS->data.yAccel);
-      Serial.print(F(" yVel: "));
-      Serial.print(myGNSS.packetUBXHNRINS->data.yAngRate);
+    if (myGNSS.getHNRPVT(125) == true){// Request HNR PVT data using a 125ms timeout
+      long velocity = myGNSS.packetUBXHNRPVT->data.speed;
+      Serial.print(F(" | Ground Speed (MPH): "));
+      Serial.print(velocity/447.04);
     }
+
+    // if (myGNSS.getHNRDyn(125) == true){ // Request HNR Dyn data using a 125ms timeout
+    //   int accel = myGNSS.packetUBXHNRINS->data.xAccel;
+    //   // Serial.print(F(" yAccel: "));
+    //   // Serial.print(myGNSS.packetUBXHNRINS->data.xAccel);
+    // }
 
     Serial.println();
   }
-
-  
 }
 
 void startupAnimation(){
-  // uint16_t hue = 230;
-  // uint32_t fillColor;
-  // for(int i=0; i<250; i+=2){
-  //   fillColor = leds.gamma32(leds.ColorHSV(hue*65536/361,250,i));
-  //   leds.fill(fillColor);
-  //   delay(1);
-  //   leds.show();
-  // }
-  // for(int i=250; i>1; i-=2){
-  //   fillColor = leds.gamma32(leds.ColorHSV(hue*65536/361,i,250));
-  //   leds.fill(fillColor);
-  //   delay(1);
-  //   leds.show();
-  // }
-  // hue = 30;
-  //  for(int i=0; i<255; i+=2){
-  //   fillColor = leds.gamma32(leds.ColorHSV(hue*65536/361,i,250));
-  //   leds.fill(fillColor);
-  //   delay(1);
-  //   leds.show();
-  // }
-  // for(int i=250; i>0; i-=2){
-  //   fillColor = leds.gamma32(leds.ColorHSV(hue*65536/361,255,i));
-  //   leds.fill(fillColor);
-  //   delay(1);
-  //   leds.show();
-  // }
+  int cycles = 0;
+  static uint8_t hue;
+
+  while(cycles<5){
+    for(int i = 0; i < LED_COUNT/2; i++) {   
+      leds.fadeToBlackBy(40);
+      leds[i] = CHSV(hue++,255,255);
+      leds(LED_COUNT/2,LED_COUNT-1) = leds(LED_COUNT/2 - 1 ,0);
+      FastLED.delay(12-(cycles*10/5));
+    }
+    cycles+=1;
+  }
+  for(int i = 0; i < LED_COUNT; i++) {  
+    leds.fadeToBlackBy(5);
+    FastLED.show();
+  }
 }
