279dcb5dcb7a1deaba7df17ed20ccd5cace58472
Derrick Ng <ahde2k@gmail.com>
2024-02-15 03:39:56 -0800

    Add files via upload

--- TeslaGPSLed_DeadReckoningFastLED/TeslaGPSLed_DeadReckoningFastLED.ino
+++ TeslaGPSLed_DeadReckoningFastLED/TeslaGPSLed_DeadReckoningFastLED.ino
@@ -5,12 +5,17 @@
 #define LED_COUNT 144 //number of leds in that array
 #define LED_PIN 32 //rgb led attached to D32
 #define SERIALECHO true
+#define TESTDATA false
+// #define SWITCHPIN 
 
 SFE_UBLOX_GNSS myGNSS;
 CRGBArray<LED_COUNT> leds;
 
 uint16_t hueValue = 0;
+int setBrightness = 155; //max 255
 // uint32_t color = leds.ColorHSV(hueValue);
+
+void(* resetFunc) (void) = 0;
 
 void setup()
 {
@@ -19,12 +24,12 @@ void setup()
   Wire.setClock(200000);
 
   Serial.begin(115200);
-  while (!Serial);
+  // while (!Serial);
   Serial.println(F("Tesla GPS DR LEDs"));
 
   if (myGNSS.begin() == false){ //Connect to the u-blox module using Wire port
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
-    while (1);
+    resetFunc();
   }
 
   myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
@@ -51,37 +56,93 @@ void setup()
   myGNSS.setNavigationFrequency(18); //Set output to 5 times a second
   myGNSS.setHNRNavigationRate(30);
   Serial.println("Current update rate: 30Hz");
+  Serial.print(myGNSS.getHour());
+  Serial.print(":");
+  Serial.print(myGNSS.getMinute());
+  Serial.println();
   startupAnimation();
 }
 
 unsigned long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
 unsigned long lastTime2 = 0;
+unsigned long lastTime3 = 0;
+
 float velocity  = 0;
+float oldVelocity = 0;
+float accel = 0;
+bool sportMode = false;
+
 
 void loop()
 {
-  if (millis() - lastTime > 15)
-  {
-    Serial.print("Lasttime: ");
-    Serial.print(millis()-lastTime);
-    lastTime = millis(); //Update the timer
-
-
-    if (myGNSS.getHNRPVT(50) == true){// Request HNR PVT data using a 125ms timeout
-      velocity = myGNSS.packetUBXHNRPVT->data.gSpeed;
-      Serial.print(F(" | Ground Speed (MPH): "));
-      Serial.print(velocity/447.04);
+  if (TESTDATA){
+    if (millis() - lastTime > 30){
+      if (SERIALECHO){
+        Serial.print("Lasttime: ");
+        Serial.print(millis()-lastTime);      
+      }
+      lastTime = millis(); //Update the timer
+      if (velocity < 35)
+        velocity = velocity+=0.6; 
+      else if (velocity < 60)
+        velocity = velocity+=0.5; 
+      else if (velocity < 80)
+        velocity = velocity+=0.4; 
+      else if (velocity < 110)
+        velocity = velocity+=0.3; 
+      else if (velocity < 150)
+        velocity = velocity+=0.1;
+      else if (velocity > 150)
+        velocity = 0;
+      
+      if (SERIALECHO){
+        Serial.print(F(" | Ground Speed (MPH): "));
+        Serial.print(velocity);
+        Serial.print(F(" | Lat Acc (Gs): "));
+        Serial.print(accel);
+        Serial.println();
+      }
+      
     }
-    // if (myGNSS.getHNRDyn(125) == true){ // Request HNR Dyn data using a 125ms timeout
-    //   int accel = myGNSS.packetUBXHNRINS->data.xAccel;
-    //   // Serial.print(F(" yAccel: "));
-    //   // Serial.print(myGNSS.packetUBXHNRINS->data.xAccel);
-    // }
-    Serial.println();
   }
+  else{
+    if (millis() - lastTime > 10){
+      if (SERIALECHO){
+        Serial.print("Lasttime: ");
+        Serial.print(millis()-lastTime);      
+      }
+      lastTime = millis(); //Update the timer
+
+      if (myGNSS.getHNRPVT(50) == true){// Request HNR PVT data using a 50ms timeout
+        velocity = myGNSS.packetUBXHNRPVT->data.gSpeed;
+        if (SERIALECHO){
+          Serial.print(F(" | Ground Speed (MPH): "));
+          Serial.print(velocity/447.04);
+          Serial.print(F(" | Lat Acc (Gs): "));
+          Serial.print(accel);
+        }
+      }
+      if(SERIALECHO)
+        Serial.println();
+    }
+  }
+
   if(millis()-lastTime2 >15){
     lastTime2 = millis();
-    fastLEDHandler(velocity/447.04, false);
+    if(TESTDATA)
+      fastLEDHandler(velocity, false);
+    else
+      fastLEDHandler(velocity/447.04, false);
+  }
+
+  if(millis()-lastTime3 > 30){ //check acceleration every 30ms
+    if(TESTDATA)
+      accel = (((velocity - oldVelocity)/2.237)/((millis()-lastTime3)/1000.0))/9.81; //conv to m/s, conv to s, conv to g's
+    else
+      accel = (((velocity - oldVelocity)/(2.237*447.04))/((millis()-lastTime3)/1000.0))/9.81;
+
+    oldVelocity = velocity; 
+    lastTime3 = millis();
   }
   // fastLEDHandler(velocity/447.04, false);
 }
@@ -114,27 +175,28 @@ void fastLEDHandler(float velocity, bool usePalette){
     index = map(velocity*100.0, 0, 16000, 0, 2550)/10.0;
   else{
     if (velocity < 60){
-      firstPixelHue = map(velocity*100.0, 0, 6000, 16000,9600)/100.0; //velocity to hue
+      firstPixelHue = map(velocity*100.0, 0, 6000, 16000,8000)/100.0; //velocity to hue
     }
     else if (velocity < 100){
-      firstPixelHue = map(velocity*100.0, 6000, 10000, 9600, 3200)/100.0;
+      firstPixelHue = map(velocity*100.0, 6000, 10000, 8000, -2000)/100.0;
     }
     else{
-      firstPixelHue = map(velocity*100.0, 10000, 14000, 3200, -4000)/100.0;
+      firstPixelHue = map(velocity*100.0, 10000, 14000, -2000, -7000)/100.0;
     }
   }
 
   for(int i = 0; i < LED_COUNT/2; i++) { 
-    int pixelHue = firstPixelHue + (i * 0.2*255 / LED_COUNT);  
+    int pixelHue = firstPixelHue + (i * 255*(0.2 + 0.1*velocity/150) / LED_COUNT);  //multiplier increase shows higher variance
     // leds.fadeToBlackBy(40);
     if (usePalette)
       leds[i] = ColorFromPalette(myPalette,index);
     else
-      leds[i] = CHSV(pixelHue,255,255);
+      leds[LED_COUNT/2 - i] = CHSV(pixelHue,255,setBrightness+(accel*70));
 
     leds(LED_COUNT/2,LED_COUNT-1) = leds(LED_COUNT/2 - 1 ,0);
     // FastLED.show();
   }
+  // if(velocity>59.50 && velocity<60.50)
   FastLED.show();
 }
 
